9055724b8bb07fa2cfe2da6a0b15e8e498269122
Pippin <penelopeburkett@gmail.com>
2021-09-20 18:37:02 +0100

    server connection established

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+import { AllFormsProvider } from './contexts/AllFormsContext';
+
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+
+
+function App() {
+  return (
+    <Router>
+      <AllFormsProvider>
+        <Switch>
+          <Route exact path ="/" component={Home}/>
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </AllFormsProvider>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AllFormsList from '../../components/AllFormsList/AllFormsList';
+
+function Home(){
+  return (
+    <>
+    <h1>Home</h1>
+    <AllFormsList/>
+    </>
+  );
+}
+
+export default Home;
